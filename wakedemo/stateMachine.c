90299c627392a686f589b9b7de68e26bf615b029
student <akarellano2@miners.utep.edu>
2021-01-14 11:04:24 -0700

    added dim lights and comments

--- wakedemo/stateMachine.c
+++ wakedemo/stateMachine.c
@@ -16,14 +16,16 @@ static int state = 0;
 
 char toggle = 0; //on or off
 char times = 0; //times led is on
-void fish()
+//char dimState = 0; // will know in which dim it is on
+
+void fish() //will change the position of fish 1
 {
   
   switch(state){
 
   case 0:
-    clearScreen(COLOR_BLUE);
-    drawFish(40,50, COLOR_ORANGE, 20);
+    clearScreen(COLOR_BLUE); //erases previous fish
+    drawFish(40,50, COLOR_ORANGE, 20); //changes position.
     state = 1;
      break;
   case 1:
@@ -49,14 +51,14 @@ void fish()
   }
 }
 
-void fish2()
+void fish2() //will change position of fish every case
 {
   
   switch(state){
 
   case 0:
-    clearScreen(COLOR_RED);
-    drawFish(40,50, COLOR_BLUE, 20);
+    clearScreen(COLOR_RED); //will erase previous fish
+    drawFish(40,50, COLOR_BLUE, 20); //same fish with new postion
     state = 1;
      break;
   case 1:
@@ -82,13 +84,13 @@ void fish2()
   }
 }
 
-void changeColor()
+void changeColor() //everytime there is a new diamond drawn
 {
   
   switch(state){
   case 1:
-    clearScreen(COLOR_GREEN);
-    drawDiamond(20,20,20, COLOR_RED);
+    clearScreen(COLOR_GREEN); //will clear the screen
+    drawDiamond(20,20,20, COLOR_RED); //draws new different diamond
     state = 2;
     break;
   case 2:
@@ -109,11 +111,11 @@ void changeColor()
   }
 }
 
-void buzzer()
+void buzzer() //used to activate buzzer
 {
   static char next  = 0;
 
-  switch(next){
+  switch(next){ //will change to see which note is played
   case 1:
     buzzer_set_period(C4);
     next++;
@@ -129,49 +131,144 @@ void buzzer()
   default:
     next++;
     break;
+  
   }
 }
 
-void toggle_green_led()
+//void toggle_green_led()
+char toggle_green_led() //will dim the light to 50%
 {
-  if(times < 70){
+  if(times < 500){ //decides how many times the method is done
     times++;
-    toggle = toggle_green(toggle);
+    toggle = toggle_green(toggle); //each time the toggle state will change
     led_update;
   }
   else{
     times = 0;
+    // dimState = 2; //added
+  }
+   return 2;
+}
+
+
+char dim25()
+{ //dim light to 25 %
+  if(times < 70){ // will keep reapeting until 70
+    times++;
+    switch(state){ // decides when the leds are turned on
+    case 0:
+      green_on = 0;
+      state = 1;
+      break;
+    case 1:
+      green_on = 0;
+      state = 2;
+      break;
+    case 2:
+      green_on = 0;
+      state = 3;
+    case 3:
+      green_on = 1;
+      state = 0;
+      break;
+   }
+    led_update();
+  }
+  else{
+    times = 0;
+    // dimState = 1;
+      }
+  return 1;
+    // led_changed = 1
+}
+
+char dim75(){ //dim light 75%
+  if(times < 70){
+    times++;
+    switch(state){ // switch cases controls how much the led lights up
+    case 0:
+      green_on = 0;
+      state = 1;
+      break;
+    case 1:
+      green_on = 1;
+      state = 2;
+      break;
+    case 2:
+      green_on = 1;
+      state = 3;
+      break;
+    case 3:
+      green_on = 1;
+      state = 0;
+      break;
+    }
+    //led_changed = 1;
+    led_update();
+  }
+  else{
+    times = 0;
+    // dimState = 3;
+  }
+  return 3;
+}
+
+
+
+
+void dimLights()
+{
+  static char dimState = 0;
+  switch(dimState){
+  case 0:       // will activate led at 25%
+    dim25();
+    dim25();
+    dim25();
+    dimState = 1;
+    break;
+  case 1:         // led to 50%
+    toggle_green_led();
+    toggle_green_led();
+    toggle_green_led();
+    dimState = 2;
+    break;
+  case 2:      // led to 50%
+    dim75();
+    dim75();
+    dim75();
+    dimState = 0;
+    break;
   }
 }
 
-void state_advance()
+void state_advance()  //changes states depending on what switch clicked.
 {
   static char next_state;
 
   switch(switch_state_down){
       case 1:
-        fish();
+        fish(); // fish that changes color
 	break;
       case 2:
 	
-	fish2();
+	fish2(); // fish with sound will activate
 	buzzer();
 	break;
-      case 3:
-        changeColor();
+     case 3:
+       changeColor(); // activate diamond with changing colors
 	//toggle_green_led();
+	//dimLights();
+	//clearScreen(COLOR_YELLOW);
 	break;
-      case 4:
-	toggle_green_led();
-	//changeColor();
-	/*clearScreen(COLOR_YELLOW);
-	  drawString11x16(5, 100, "BYE", COLOR_RED, COLOR_BLUE);*/
-	//horizontal, vertical
+     case 4: // will turn on green led   
+	dimLights();
       	break;
-  }
+  }      
 }
 
-    
+
+
+  
 
 
 
@@ -180,44 +277,12 @@ void state_advance()
 
 
 
-    
-/*    
-char toggle_green() //green will blink only
-{
-  static char state = 0;
-  switch(state){
-  case 0:
-    red_on = 1;
-    state = 1;
-    break;
-  case 1:
-    red_on = 0;
-    state = 0;
-    break;
-  }
-  return 1;
- 
-}
 
-char red_blink() // red light will blink
-{
-  static char state = 0;
-  switch(state){
 
-  case 0:
-    green_on = 1;
-    // red_on = 0;
-    state = 1;
-    break;
-  case 1:
-    // green_on = 0;
-    green_on = 0;
-    state = 0;
-    break;
-  }
-  return 1;
-}
 
+
+
+  /*
 char alternate(){ // switch between both
   char changed = 0;
   static enum {R = 0, G = 1} color = G;
@@ -228,7 +293,7 @@ char alternate(){ // switch between both
   led_changed = changed;
   led_update();
 }
-
+*//*
 void dim25(){ //dim light to 25 %
   static char dimState = 0;
   switch(dimState){
@@ -251,7 +316,7 @@ void dim25(){ //dim light to 25 %
   led_changed = 1;
   led_update();
 }
-
+/*
 void dim50(){ // dim light 50 %
   static char dimState = 0;
   switch(dimState){
@@ -275,7 +340,8 @@ void dim50(){ // dim light 50 %
   led_changed = 1;
   led_update();
 }
-
+*/
+/*
 void dim75(){ //dim light 75%
   static char dimState = 0;
   switch(dimState){
@@ -298,5 +364,5 @@ void dim75(){ //dim light 75%
   }
   led_changed = 1;
   led_update();
-}
-*/
+  }*/
+
--- wakedemo/stateMachine.h
+++ wakedemo/stateMachine.h
@@ -8,9 +8,9 @@ void state_advance();
 void dim_leds(int x);
 char toggle_green();
 char red_blink();
-void dim25();
-void dim50();
-void dim75();
-
-
+char dim25();
+char dim50();
+char dim75();
+char toggle_green_led();
+void dimLights();
 #endif
