524bbbefceb530a993ddf96d0a2e92dbde0c15fa
student <akarellano2@miners.utep.edu>
2021-01-14 11:05:02 -0700

    added comments and fixed error

--- wakedemo/toggle_green.s
+++ wakedemo/toggle_green.s
@@ -16,23 +16,23 @@ jt:
 toggle_green:
 	
 	cmp.b #2, &toggle
-	jhs end			;state > 1
+	jhs end			;state > 2 cant use cases
 
 	
 	mov &toggle,r12
 	add.b r12,r12		;2 * state
-	mov jt(r12),r0		;jump to jt
+	mov jt(r12),r0		;jt+r12 will go to pc and goes to next step
 
 case0:
-	mov.b 1, &green_on
-	mov.b 1, &toggle
+	mov.b 1, &green_on	;turning on led
+	mov.b 1, &toggle	;changing state
 	jmp end 		;break
 
 case1:
-	mov.b 0, &green_on
-	mov.b 0, r12
+	mov.b 0, &green_on	;turning off led
+	mov.b 0, &toggle	;changing state
 	jmp end			;break
 
 end:
 	mov.b #1, r13		;return 1
-	pop r0
+	pop r0			;you go to next instruction that is next
